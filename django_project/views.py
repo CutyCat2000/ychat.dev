d3011faefca8afaec93714d180f2d3c5969a91c3
CutyCat2000 <132785498+CutyCat2000@users.noreply.github.com>
2023-09-25 18:47:19 +0200

    v1.1

--- django_project/views.py
+++ django_project/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.shortcuts import get_object_or_404
 from dm.models import DM
 from django.contrib.auth.models import User
@@ -9,4 +9,4 @@ def home(request):
         user = get_object_or_404(User, pk=request.user.id)
         dms = DM.objects.filter(user_1=user) | DM.objects.filter(user_2=user)
         return render(request, "index.html", {"dms": dms})
-    return render(request, "index.html")
+    return redirect("user/login")
