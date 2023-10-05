f57cf072ba65bf8105fba4e126592838b20662e6
CutyCat2000 <132785498+CutyCat2000@users.noreply.github.com>
2023-10-05 20:31:40 +0200

    Update config.py

--- config.py
+++ config.py
@@ -1,3 +1,15 @@
+# Just some normal settings
 NAME = "YChat"
 WEBSITE = "ychat.dev"
 ICON = "/static/icon.png"
+
+# Make a random secret key for your project
+SECRET_KEY = "REQUIRED"
+
+# For ReCaptcha, please go to https://www.google.com/recaptcha/admin/create
+RECAPTCHA_PUBLIC_KEY = 'REQUIRED'
+RECAPTCHA_PRIVATE_KEY = 'REQUIRED'
+
+# Allow to send [x] messages per [x] seconds per user
+MESSAGE_LIMIT = 15
+MESSAGE_DELAY = 60
